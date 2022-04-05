80eab3087de3d03681086094a2f8f36d6030c8b1
InverseDa <inverse_boom@outlook.com>
2022-04-05 15:56:43 +0800

    Initial commit

---
+++ src/main/java/com/hpbe/highperspicacitybe/HighPerspicacityBeApplication.java
@@ -0,0 +1,13 @@
+package com.hpbe.highperspicacitybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPerspicacityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighPerspicacityBeApplication.class, args);
+    }
+
+}
